8bfd8babd2c6256accc16da41fef26f55695be3e
Michael Kowal <Mchlkwl@gmail.com>
2018-09-29 02:20:05 -0700

    initial commit.  Added all of the WIP.

---
+++ cmake-build-debug/CMakeFiles/RayTracing.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RayTracing.dir/src/main.cpp.o"
+  "CMakeFiles/RayTracing.dir/src/vec3.cpp.o"
+  "CMakeFiles/RayTracing.dir/src/ray.cpp.o"
+  "RayTracing.pdb"
+  "RayTracing"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayTracing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/vec3.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Michael Kowal on 2018-09-29.
+//
+
+#include "vec3.h"
